f1ea100bf35fe9ad7093c296df4907450862c64e
Gabriel BF <gbbolonhez@gmail.com>
2025-02-10 23:33:19 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Button from "./components/Button";
+
+function App() {
+  return ( 
+  <div className="App">
+    <h1>Hi</h1>
+    <Button title="Enter"></Button>
+    <Button title="Close"></Button>
+    <Button title="Open"></Button>
+    <Button title="Save"></Button>
+  </div>
+  );
+}
+
+export default App;
